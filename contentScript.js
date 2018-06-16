76081c6dc3be4b657be6455d2011ab5eefa37f76
Daniel Leone <mail.dleone@gmail.com>
2018-06-16 13:19:02 +0800

    adding some sweet rainbow animations to the ship it button

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,21 @@
-console.log("ship it already mate!");
+console.log("it's probably fine, just ship it!");
+var style = document.createElement('style');
+style.type = 'text/css';
+style.appendChild(document.createTextNode(`
+    @keyframes rainbow { 
+        0%{background-position:0% 82%}
+        50%{background-position:100% 19%}
+        100%{background-position:0% 82%}
+    }
+    
+    #fulfill-pullrequest:hover {
+        animation: rainbow 0.5s ease infinite !important;
+        background: linear-gradient(124deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3) !important;
+        background-size: 1800% 1800% !important;
+    }
+`));
+document.head.appendChild(style);
+
 var shipItBtn = document.getElementById('fulfill-pullrequest');
 var approveBtn = document.getElementById('approve-button');
 if (shipItBtn) {
